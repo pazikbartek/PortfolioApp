b11be5a25fb6a4bfe757aa116dbb79447b5a6aa9
bartek <bartosz.pazik98@gmail.com>
2019-04-18 23:53:22 +0200

    photos and skills view

--- src/menu.js
+++ src/menu.js
@@ -4,7 +4,7 @@ class Menu extends Component{
     render(){
 
         return(
-            <div class="menu">
+            <div className="menu">
                 <div className="button" onClick={this.props.handleClick} id="Aboutme">O mnie</div>
                 <div className="button" onClick={this.props.handleClick} id="Skills">Umiejętności</div>
                 <div className="button" onClick={this.props.handleClick} id="Projects">Projekty</div>
--- src/skills.js
+++ src/skills.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 import Menu from './menu'
+import html from './photos/html.png'
+import css from './photos/css.png';
+import js from './photos/js.png'
+import webpack from './photos/webpack.png';
+import react from './photos/react.png';
+import git from './photos/git.png';
+
 
 class Skills extends Component{
     render(){
         return(
             <div>
                 <Menu handleClick={this.props.handleClick}/>
-                <div>
-                    
+                <div className="technologies">
+                    <div className="techname">
+                        <h3>HTML</h3>
+                        <img src={html} />
+                    </div>
+                    <div className="techname">
+                        <h3>CSS</h3>
+                        <img src={css} />
+                    </div>
+                    <div className="techname">
+                        <h3>JavaScript</h3>
+                        <img src={js} />
+                    </div>
+                    <div className="techname">
+                        <h3>React</h3>
+                        <img src={react} />
+                    </div>
+                    <div className="techname">
+                        <h3>GIT</h3>
+                        <img src={git} />
+                    </div>
+                    <div className="techname">
+                        <h3>Webpack</h3>
+                        <img src={webpack} />
+                    </div>
+                
                 </div>
             </div>
         )
