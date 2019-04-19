4d0c70548f64e43e8182646b94d72f12cd1ae09a
bartek <bartosz.pazik98@gmail.com>
2019-04-19 20:06:44 +0200

    Updates

---
+++ precache-manifest.75cb37d8d6ddbd7a44ec373a392e3a75.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "eed1bf89e62bec2b3e04660d7ce1b167",
+    "url": "/PortfolioApp/static/media/react.eed1bf89.png"
+  },
+  {
+    "revision": "2b9cdfea5baa6784b8c6",
+    "url": "/PortfolioApp/static/css/main.15fc0996.chunk.css"
+  },
+  {
+    "revision": "1b5033fe105c05ef6ce6",
+    "url": "/PortfolioApp/static/js/runtime~main.09c0c2aa.js"
+  },
+  {
+    "revision": "13fe89eb2cc5897f0be7",
+    "url": "/PortfolioApp/static/js/2.1d3f6ac8.chunk.js"
+  },
+  {
+    "revision": "666fcef4a33ec77bbeac654a0d600919",
+    "url": "/PortfolioApp/static/media/html.666fcef4.png"
+  },
+  {
+    "revision": "043e307cbcc1118c87cff716448fed4c",
+    "url": "/PortfolioApp/static/media/css.043e307c.png"
+  },
+  {
+    "revision": "da21f98fa1be37595316386366457672",
+    "url": "/PortfolioApp/static/media/js.da21f98f.png"
+  },
+  {
+    "revision": "b282a86be6988a2cbfb7eabe6c254d12",
+    "url": "/PortfolioApp/static/media/webpack.b282a86b.png"
+  },
+  {
+    "revision": "2b9cdfea5baa6784b8c6",
+    "url": "/PortfolioApp/static/js/main.33c109b7.chunk.js"
+  },
+  {
+    "revision": "64ecdacfac2a8483e99114692dda664f",
+    "url": "/PortfolioApp/static/media/git.64ecdacf.png"
+  },
+  {
+    "revision": "05e1f6ec120ee47106937fff84a92524",
+    "url": "/PortfolioApp/static/media/gts.05e1f6ec.jpg"
+  },
+  {
+    "revision": "ec7d20308c63ea756489bdbe309939ac",
+    "url": "/PortfolioApp/static/media/fande.ec7d2030.jpg"
+  },
+  {
+    "revision": "48cf44d8f23b1b866ab3dde467fd6db6",
+    "url": "/PortfolioApp/static/media/todo.48cf44d8.jpg"
+  },
+  {
+    "revision": "1474aeeccdd9c126d0a4b97ed9ad01b5",
+    "url": "/PortfolioApp/static/media/rentcar.1474aeec.jpg"
+  },
+  {
+    "revision": "d82f572ecbb923e5bc6c4ecfb8d18d85",
+    "url": "/PortfolioApp/static/media/tlo.d82f572e.jpg"
+  },
+  {
+    "revision": "cef97dbfdcdb8e479a7b980230cfb60c",
+    "url": "/PortfolioApp/index.html"
+  }
+];
